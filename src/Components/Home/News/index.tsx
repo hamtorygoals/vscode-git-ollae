a4b606358feda57fe67f5942b7a1281b20615a2a
yunseochnn <10cys10@gmail.com>
2024-02-11 22:58:44 +0900

    [feat] 올래 생활뉴스 complete && 검색 ui 수정

--- src/Components/Home/News/index.tsx
+++ src/Components/Home/News/index.tsx
@@ -1,11 +1,13 @@
 import Header from '../Header';
+import { useNavigate } from 'react-router-dom';
 import { truncate } from '../../../utils/truncate';
 import { fromNow } from '../../../utils/dayjs';
 import { useGetNewsQuery } from '../../../redux/apis/newsApi';
 
 const News = () => {
+  const navigate = useNavigate();
   let content;
-  const { data, isLoading, error } = useGetNewsQuery();
+  const { data, isLoading, error } = useGetNewsQuery({});
 
   if (isLoading) {
     content = <p>Loading...</p>;
@@ -19,21 +21,53 @@ const News = () => {
     const newsData = data?.result.newsList.slice(0, 3);
 
     content = (
-      <div className="flex flex-col gap-5">
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '16px',
+        }}
+      >
         {newsData.map((news, index) => (
           <div
             key={news.id}
-            className="flex gap-5 p-2 overflow-hidden transition-all cursor-pointer"
+            style={{
+              display: 'flex',
+              width: '563px',
+              height: '119px',
+              padding: '8px',
+              overflow: 'hidden',
+              transition: 'all',
+              cursor: 'pointer',
+            }}
+            onClick={() => navigate(`/news/${news.id}`)}
           >
             <img
               // src={news.filePath}
               // alt={news.filePath}
               src={`/assets/News/news${index + 1}.png`}
               alt="newsImg"
-              className="object-cover rounded-md"
+              style={{
+                width: '103px',
+                height: '103px',
+                borderRadius: '16px',
+              }}
             />
-            <span className="flex flex-col gap-1">
-              <p className="font-medium text-primary01">
+            <span
+              style={{
+                marginLeft: '12px',
+              }}
+            >
+              <p
+                style={{
+                  height: '17px',
+                  fontSize: '14px',
+                  fontWeight: '500',
+                  color: '#EC9D26',
+                  lineHeight: '16.71px',
+                  marginBottom: '8px',
+                }}
+              >
                 {news.category === 'BOARD'
                   ? '공지사항'
                   : news.category === 'HEALTH'
@@ -42,22 +76,66 @@ const News = () => {
                       ? '생활정보'
                       : '취업정보'}
               </p>
-              <p className="text-lg font-medium hover:underline hover:decoration-4 w-fit">
+              <p
+                style={{
+                  width: '443px',
+                  height: '21px',
+                  color: '#1A1A1A',
+                  fontSize: '18px',
+                  lineHeight: '21.48px',
+                  fontWeight: '500',
+
+                  marginBottom: '8px',
+                  cursor: 'pointer',
+                }}
+                className="hover:underline hover:decoration-4 w-fit"
+              >
                 {news.title}
               </p>
-              <p className="text-slate-500">{truncate(news.content, 38)}</p>
-              <section id="writer" className="flex items-center gap-2 mt-1">
+              <p
+                style={{
+                  height: '19px',
+                  width: '432px',
+                  fontSize: '16px',
+                  marginBottom: '6px',
+                  color: '#808080',
+                  lineHeight: '19.09px',
+                }}
+              >
+                {truncate(news.content, 38)}
+              </p>
+              <section
+                className="author"
+                style={{
+                  width: 'auto',
+                  height: '24px',
+                  fontSize: '12px',
+                  display: 'flex',
+                  alignItems: 'center',
+                }}
+              >
                 <img
                   // src={news.writerProfile}
                   // alt={news.writerProfile}
                   src={`/assets/Teacher/teacher${index + 1}.png`}
                   alt="authorImg"
-                  className="object-cover w-6 h-6 rounded-full"
+                  style={{
+                    width: '24px',
+                    height: '24px',
+                    borderRadius: '50%',
+                    marginRight: '8px',
+                  }}
                 />
-                <p className="text-sm font-medium text-slate-500">
+                <p
+                  style={{
+                    marginRight: '8px',
+                    color: '#808080',
+                    lineHeight: '14.32px',
+                  }}
+                >
                   {news.author}
                 </p>
-                <p className="text-sm font-medium text-slate-300">
+                <p style={{ color: '#B3B3B3', lineHeight: '14.32px' }}>
                   {fromNow(news.createdAt)}
                 </p>
               </section>
@@ -70,8 +148,13 @@ const News = () => {
 
   return (
     <div className="">
-      <Header partName="올래 생활 뉴스" />
-      <hr className="w-full h-[2px] bg-black mt-2 mb-5" />
+      <Header partName="올래 생활 뉴스" pathName="news" />
+      <hr
+        style={{
+          marginBottom: '24px',
+        }}
+        className="w-full h-[2px] bg-black mt-2"
+      />
       {content}
     </div>
   );
--- src/Components/Navbar/Header.tsx
+++ src/Components/Navbar/Header.tsx
@@ -11,16 +11,34 @@ const Header = () => {
           className="object-cover w-20 h-8 cursor-pointer"
           onClick={() => navigate('/')}
         />
-        <section className="relative">
+        <section style={{ display: 'flex', position: 'relative' }}>
           <input
-            className="px-4 py-2 border rounded-full outline-none text-primary01 border-primary01 w-[540px] placeholder-primary01"
+            className="placeholder-primary01"
+            style={{
+              padding: '12px 66px 11px 16px',
+              border: '1px solid #CCCCCC',
+              borderRadius: '20px',
+              outline: 'none',
+              color: '#EC9D26',
+              borderColor: '#EC9D26',
+              width: '540px',
+              fontSize: '14px',
+            }}
             type="text"
             placeholder="배우고 싶은 분야가 있나요? 제목/강사명/수업 내용으로 검색해 보세요!"
           />
           <img
             src="/assets/Utils/search.svg"
             alt="search"
-            className="absolute w-6 h-6 cursor-pointer right-4 top-2"
+            style={{
+              width: '24px',
+              height: '24px',
+              cursor: 'pointer',
+              position: 'absolute',
+              top: '50%',
+              right: '16px',
+              transform: 'translateY(-50%)',
+            }}
           />
         </section>
       </span>
